5ae86e73a9a33597c112d0098c6c98bfb795b48a
John Chishugi <jkchishugi@gmail.com>
2021-10-21 18:22:49 +0100

    ft-firebase-config-file

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSN8CtRajebpkN25ugPM3qb8wis5Eq8B4",
+    authDomain: "net-ninja-marioplan-b43c9.firebaseapp.com",
+    projectId: "net-ninja-marioplan-b43c9",
+    storageBucket: "net-ninja-marioplan-b43c9.appspot.com",
+    messagingSenderId: "358052816656",
+    appId: "1:358052816656:web:6abfb14b4a04d7059da795",
+    measurementId: "G-GR2DNZSNQY"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
