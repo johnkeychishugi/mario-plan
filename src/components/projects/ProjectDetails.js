f29eaba80941fecb7e8d25dcc8e77fa4587cdb00
John Chishugi <jkchishugi@gmail.com>
2021-10-21 15:42:09 +0100

    ft-project-detail-component

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
- const  ProjectDetails = () => {
+ const  ProjectDetails = (props) => {
+    const id = props.match.params.id;
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Project Title</span>
+                    <span className="card-title">Project Title - {id}</span>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum omnis assumenda in nulla deleniti sed iure ipsum laborum quos temporibus alias nostrum consequuntur accusamus vitae voluptates, eum dolorem iste. Provident?</p>
                 </div>
-                <div className="card-acton grey lighten-4 grey-text">
+                <div className="card-action gret lighten-4 grey-text">
                     <div>Posted by the Net Ninja</div>
                     <div>2nd September, 2am</div>
                 </div>
