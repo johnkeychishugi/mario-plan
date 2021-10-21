7635b5d89b0dc763831bc2c29885bf7492e79217
John Chishugi <jkchishugi@gmail.com>
2021-10-21 15:32:01 +0100

    ft-project-details-component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,29 +1,13 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-deph-0 project-summary">
-                <div className="card-content grey-text text-draken-3">
-                    <span className="card-title">Porject Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">3rd september, 2am</p>
-                </div>
-            </div>
-            <div className="card z-deph-0 project-summary">
-                <div className="card-content grey-text text-draken-3">
-                    <span className="card-title">Porject Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">3rd september, 2am</p>
-                </div>
-            </div>
-            <div className="card z-deph-0 project-summary">
-                <div className="card-content grey-text text-draken-3">
-                    <span className="card-title">Porject Title</span>
-                    <p>Posted by the Net Ninja</p>
-                    <p className="grey-text">3rd september, 2am</p>
-                </div>
-            </div>
+           <ProjectSummary/>
+           <ProjectSummary/>
+           <ProjectSummary/>
+           <ProjectSummary/>
         </div>
       );
 }
