801c7057d399059ac5e8e270b054d5af63c7e503
John Chishugi <jkchishugi@gmail.com>
2021-10-21 15:08:21 +0100

    ft-create-dashboard-components

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-deph-0 project-summary">
+                <div className="card-content grey-text text-draken-3">
+                    <span className="card-title">Porject Title</span>
+                    <p>Posted by the Net Ninja</p>
+                    <p className="grey-text">3rd september, 2am</p>
+                </div>
+            </div>
+            <div className="card z-deph-0 project-summary">
+                <div className="card-content grey-text text-draken-3">
+                    <span className="card-title">Porject Title</span>
+                    <p>Posted by the Net Ninja</p>
+                    <p className="grey-text">3rd september, 2am</p>
+                </div>
+            </div>
+            <div className="card z-deph-0 project-summary">
+                <div className="card-content grey-text text-draken-3">
+                    <span className="card-title">Porject Title</span>
+                    <p>Posted by the Net Ninja</p>
+                    <p className="grey-text">3rd september, 2am</p>
+                </div>
+            </div>
+        </div>
+      );
+}
+ 
+export default ProjectList;
