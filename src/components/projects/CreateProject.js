150684379e6963456bfa1ea9ff2470d9b7573e3f
John Chishugi <jkchishugi@gmail.com>
2021-10-22 13:05:06 +0100

    ft-code-format

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import { createProject } from '../../store/actions/project';
+import { createProject } from '../../store/actions/projectAction';
 class CreateProject extends Component {
     state = {
         title:'',
