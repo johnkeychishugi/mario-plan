cc9510009757a50726e1b9779189b2c0a7557412
John Chishugi <jkchishugi@gmail.com>
2021-10-22 19:02:49 +0100

    ft-protect-component-required-auth

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
+
 
 const ProjectDetails = (props) => {
     // const id = props.match.params.id;
     // console.log(props);
-    const { project } = props;
+    const { project, auth } = props;
+    
+    if(!auth.uid)  return <Redirect to="/signin" />
+
     if (project) {
         return(
             <div className="container section project-details">
@@ -36,9 +41,11 @@ const mapStateToProps = (state, ownprops) => {
     // console.log(state);
     const id = ownprops.match.params.id;
     const projects = state.firestore.data.projects;
+    const auth = state.firebase.auth;
     const project = projects ? projects[id] : null;
     return {
-        project: project
+        project: project,
+        auth: auth
     }
 }
 
