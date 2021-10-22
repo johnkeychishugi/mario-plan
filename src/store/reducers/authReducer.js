a1d5f4c44a8999d60be60391f0550276b56a4537
John Chishugi <jkchishugi@gmail.com>
2021-10-22 18:06:59 +0100

    ft-reducer-auth-login

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,5 +1,6 @@
 const initState = {
-    authError: null
+    authError: null,
+    errorMessage: null
 }
 
 const authReducer = (state = initState,action) =>{
@@ -8,14 +9,25 @@ const authReducer = (state = initState,action) =>{
             console.log('Login failed', action.err);
             return {
                 ...state,
+                errorMessage:action.err.message,
                 authError: 'Login failed'
             }
         case 'LOGIN_SUCCESS':
             console.log('Login success');
             return {
                 ...state,
-                authError: null
+                authError: null,
+                errorMessage: null
             }
+        case 'SIGNOUT_SUCCESS':
+            console.log('Sign out');
+            return state
+        case 'SIGNOUT_ERROR':
+            return {
+                ...state,
+                errorMessage:action.err,
+                authError: 'sign out failed'
+            }   
         default:
             return state;
     }
