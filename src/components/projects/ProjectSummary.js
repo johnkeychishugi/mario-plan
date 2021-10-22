6615d8e2894c80072479cf9ea3fcf7129e1a1d0c
John Chishugi <jkchishugi@gmail.com>
2021-10-22 21:43:20 +0100

    ft-project-created-redirect

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ const ProjectSummary = ({ project }) => {
             <div className="card-content grey-text text-draken-3">
                 <span className="card-title">{ project.title }</span>
                 <p>Posted by the { project.authorFirstName}  { project.authorLastName}</p>
-                <p className="grey-text">3rd september, 2am</p>
+                <p className="grey-text">{ project.createdAt.toDate().toString() }</p>
             </div>
         </div>
     );
