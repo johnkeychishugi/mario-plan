565e172acef76bc8639a768af51883747d37ce91
John Chishugi <jkchishugi@gmail.com>
2021-10-22 21:32:55 +0100

    ft-create-project

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({ project }) => {
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-draken-3">
                 <span className="card-title">{ project.title }</span>
-                <p>Posted by the Net Ninja</p>
+                <p>Posted by the { project.authorFirstName}  { project.authorLastName}</p>
                 <p className="grey-text">3rd september, 2am</p>
             </div>
         </div>
