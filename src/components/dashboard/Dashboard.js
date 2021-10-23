deda3cb0ef77075ab93b2fc38a6422e09476a900
John Chishugi <jkchishugi@gmail.com>
2021-10-23 23:22:01 +0100

    ft-code-format

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,24 +1,24 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import ProjectList from "../projects/ProjectList";
 import Notifications from "./Notifications";
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
-class Dashboard extends Component{
-    render(){
+class Dashboard extends Component {
+    render() {
         const { projects, auth, notifications } = this.props;
-         
-        if(!auth.uid)  return <Redirect to="/signin" />
 
-        return(
+        if (!auth.uid) return <Redirect to="/signin" />
+
+        return (
             <div className="dashboard container">
                 <div className="row">
                     <div className="col s12 m6">
-                        <ProjectList projects={projects}/>
+                        <ProjectList projects={projects} />
                     </div>
                     <div className="col s12 m5 offset-m1">
-                        <Notifications notifications={notifications}/>
+                        <Notifications notifications={notifications} />
                     </div>
                 </div>
             </div>
@@ -26,19 +26,19 @@ class Dashboard extends Component{
     }
 }
 
-const mapStateToProps = (state) =>{
+const mapStateToProps = (state) => {
     console.log(state);
-    return{
-        projects : state.firestore.ordered.projects,
+    return {
+        projects: state.firestore.ordered.projects,
         auth: state.firebase.auth,
-        notifications : state.firestore.ordered.notifications
+        notifications: state.firestore.ordered.notifications
     }
 }
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'projects', orderBy: ['createdAt','desc'] },
-        { collection: 'notifications', limit : 3, orderBy : ['time','desc']}
-    ]) 
+        { collection: 'projects', orderBy: ['createdAt', 'desc'] },
+        { collection: 'notifications', limit: 3, orderBy: ['time', 'desc'] }
+    ])
 )(Dashboard);
